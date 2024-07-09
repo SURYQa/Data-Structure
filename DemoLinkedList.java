b257dd40e4f0990392cfecbaed7ad58fca8f96c5
Surya M <146011503+SURYQa@users.noreply.github.com>
2024-07-09 06:50:30 +0530

    DemoLinkedList.java

--- DemoLinkedList.java
+++ DemoLinkedList.java
@@ -1,5 +1,29 @@
 package org.linkedlist;
-
+import java.util.*;
+public class Demo1 {
+	public static void main(String[] args) {
+		DoublyLinkedList list = new DoublyLinkedList();
+		
+		//list.deleteAtpos(1); // This line throw the Exception 
+		list.insertAtBegining(3);
+		list.insertAtBegining(2);
+		list.insertAtBegining(1);
+		                       
+		
+		list.display();
+		System.out.println("");
+		list.insertAtPos(0,10);
+		list.display();
+		
+		list.deleteAtpos(0);
+		System.out.println("");
+		list.display();
+		System.out.println("");
+		list.displayRev();
+		
+	}
+	
+}
 public class Demo {
 	public static void main(String[] args) {
 		
